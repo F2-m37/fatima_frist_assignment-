e2bd9df9cc57d84389f983015b7f9fb7193a2a26
F2-m37 <fm9212547@gmail.com>
2024-12-15 13:17:59 -0800

    First Assignment

--- lib/fatima_home_page.dart
+++ lib/fatima_home_page.dart
@@ -8,81 +8,201 @@ class FatimaForm extends StatefulWidget {
 }
 
 class _FatimaFormState extends State<FatimaForm> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Fatina App"),
+      backgroundColor: const Color(0XFFD5DEEF),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                width: 400,
+                height: 400,
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color(0XFF628ECB),
+                      Color(0XFF4B6FA3),
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: const Color(0XFF395886),
+                    width: 1,
+                  ),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      spreadRadius: 0,
+                      blurRadius: 8,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                padding: const EdgeInsets.all(20.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const SizedBox(
+                        height: 45,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Fatima Name",
+                            hintStyle: TextStyle(
+                              color: Color(0XFF8FA6C9),
+                            ),
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Color(0XFFD5DEEF),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        height: 45,
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            hintText: "Fatima Phone",
+                            hintStyle: TextStyle(
+                              color: Color(0XFF8FA6C9),
+                            ),
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Color(0XFFD5DEEF),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        height: 45,
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            hintText: "Fatima Age",
+                            hintStyle: TextStyle(
+                              color: Color(0XFF8FA6C9),
+                            ),
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Color(0XFFD5DEEF),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      SizedBox(
+                        height: 45,
+                        child: TextField(
+                          decoration: const InputDecoration(
+                            hintText: "Fatima Email",
+                            hintStyle: TextStyle(
+                              color: Color(0XFF8FA6C9),
+                            ),
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            fillColor: Color(0XFFD5DEEF),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 0),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Container(
+                        height: 40,
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              Color(0XFF628ECB),
+                              Color(0XFF4B6FA3),
+                            ],
+                          ),
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Colors.black26,
+                              spreadRadius: 0,
+                              blurRadius: 8,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0XFF395886),
+                            shadowColor: Colors.transparent,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: const Text(
+                            'OK',
+                            style: TextStyle(
+                              color: Color(0XFFD5DEEF),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: -40,
+                left: 0,
+                right: 0,
+                child: Center(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Color(0XFF628ECB),
+                          Color(0XFF4B6FA3),
+                        ],
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black26,
+                          spreadRadius: 0,
+                          blurRadius: 8,
+                          offset: Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: const Icon(
+                      Icons.person,
+                      size: 70,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: container(
-        mainAxisAlignment: MainAxisAlignment.center
-        width:400,
-        height:600,
-        padding: EgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: Colors.grey,
-          boxShadow:[
-            BoxShadow(color: Colors.black ,blurRadius: 5 , offset:Offset(0,2))
-          ]
-          border: Border.all(),
-          borderRadius: BorderRadius.circulal(10),
-          alignment: Alignment.center,
-          child: Column(children:[
-            TextFromField(
-              decoration: InputDecration(
-                hintText: "Fatima Name",
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width: 1)
-                )
-              ),
-            ),
-            SizedBox(height:15), 
-
-            TextFromField(
-              decoration: InputDecration(
-                hintText: "Fatima phone",
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width: 1)
-                )
-              ),
-            ),
-
-            SizedBox(height:15), 
-
-            TextFromField(
-              decoration: InputDecration(
-                hintText: "Fatima age",
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width: 1)
-                )
-              ),
-            ),
-
-            SizedBox(height:15), 
-
-            TextFromField(
-              decoration: InputDecration(
-                hintText: "Fatima email",
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(width: 1)
-                )
-              ),
-            ),
-
-            SizedBox(height:15),
-            
-            Container(
-              child: RaisedButton(
-                color: Color.white,
-                onPressed: () {},
-                child: Text("Ok",
-                style:TextStyle( color: Colors.black ,fontSize: 16))
-              )
-            )
-
-          ],))
-        
-      )
     );
   }
 }
